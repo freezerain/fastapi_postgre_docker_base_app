472cbcfcee703d381f61bbaf91300b32d30ac8a9
Freeze <rain-ilia@rambler.ru>
2023-05-19 16:33:20 +0200

    Minimal Postgre docker

---
+++ app/db.py
@@ -0,0 +1,33 @@
+# app/db.py
+import datetime
+
+import databases
+import ormar
+import sqlalchemy
+
+from .config import settings
+
+database = databases.Database(settings.db_url)
+metadata = sqlalchemy.MetaData()
+
+
+class BaseMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
+
+
+class Task(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "tasks"
+
+    id: int = ormar.Integer(primary_key=True)
+    title: str = ormar.String(max_length=128, unique=True, nullable=False)
+    description: str = ormar.Boolean(default=None, nullable=True)
+    creating_date: datetime.date = ormar.Date(default=datetime.datetime.now(), nullable=False)
+    end_date: datetime.date = ormar.Date(default=datetime.datetime.now(), nullable=False)
+    # placeholder until user table exists
+    author: str = ormar.String(max_length=128, nullable=False)
+
+
+engine = sqlalchemy.create_engine(settings.db_url)
+metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -2,9 +2,26 @@
 
 from fastapi import FastAPI
 
-app = FastAPI(title="FastAPI, Docker, and Traefik")
+from app.db import database, Task
+
+
+app = FastAPI(title="Entropy api using FastAPI, PostgreSQL and Docker")
 
 
 @app.get("/")
-def read_root():
-    return {"hello": "world"}
+async def read_root():
+    return await Task.objects.all()
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await Task.objects.get_or_create(title="dummy title", author="debug_python_code")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
