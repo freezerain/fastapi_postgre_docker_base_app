a2136ee9890ac7cb150dcf0c4e74ac102b9c22cd
Freeze <rain-ilia@rambler.ru>
2023-05-21 20:43:08 +0200

    Mixins for data model

--- app/db.py
+++ app/db.py
@@ -11,22 +11,30 @@ database = databases.Database(settings.db_url)
 metadata = sqlalchemy.MetaData()
 
 
+# Data model mixins
+class AuditMixin:
+    created_by: str = ormar.String(max_length=100)
+    updated_by: str = ormar.String(max_length=100, default="")
+
+
+class DateFieldsMixins:
+    created_date: datetime.datetime = ormar.DateTime(default=datetime.datetime.now)  # pass without "()"
+    updated_date: datetime.datetime = ormar.DateTime(default=datetime.datetime.now)
+
+
 class BaseMeta(ormar.ModelMeta):
     metadata = metadata
     database = database
 
 
-class Task(ormar.Model):
+class Task(ormar.Model, DateFieldsMixins, AuditMixin):
     class Meta(BaseMeta):
         tablename = "tasks"
 
-    id: int = ormar.Integer(primary_key=True)
-    title: str = ormar.String(max_length=128, unique=True, nullable=False)
-    description: str = ormar.String(max_length=1024, default=None, nullable=True)
-    creating_date: datetime.date = ormar.Date(default=datetime.datetime.now(), nullable=False)
-    end_date: datetime.date = ormar.Date(default=datetime.datetime.now(), nullable=False)
-    # placeholder until user table exists
-    author: str = ormar.String(max_length=128, nullable=False)
+    id: int = ormar.Integer(primary_key=True)  # autoincrement = True by default
+    title: str = ormar.String(max_length=128, unique=True, nullable=False)  # Required field
+    description: str = ormar.String(max_length=1024, default="")
+    end_date: datetime.date = ormar.DateTime(default=datetime.datetime.now() + datetime.timedelta(hours=1))
 
 
 engine = sqlalchemy.create_engine(settings.db_url)
